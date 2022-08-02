b036c484401207c2905bf77c0737bd933e413f99
aframuz <aframuz@tutanota.io>
2022-08-01 22:40:38 -0400

    cleaning

--- db/queries.js
+++ db/queries.js
@@ -12,27 +12,21 @@ const pool = new Pool(db_config)
 
 // General connection to database function
 const queryStructure = async (query, log, errMsg) => {
-   // Acquire a client from the pool
    const client = await pool.connect()
 
    try {
-      // Execute query, use log function to print adequate message
       const res = await client.query(query)
       log(res)
    } catch (err) {
-      // (3) Print error message
       console.log(`Error ${errMsg}:\nError code: ${err.code}\nError message: ${err.message}`)
    } finally {
-      // Release the client back to the pool
       client.release()
    }
-   // Close pool at the end of app
    pool.end()
 }
 
 // (1) & (2) Query configuration
 const queryConf = {
-   // Add config
    add: {
       query: {
          text: `INSERT INTO "Student" (nombre, rut, curso, nivel) VALUES ($1, $2, $3, $4) RETURNING *;`,
@@ -40,7 +34,6 @@ const queryConf = {
       log: (res) => console.log(`Estudiante ${res.rows[0].nombre} agregado con éxito`),
       errMsg: `adding student`,
    },
-   // Get config
    get: {
       query: {
          rowMode: "array",
@@ -50,7 +43,6 @@ const queryConf = {
 
       errMsg: `retrieving Students`,
    },
-   // Get by rut config
    getByRut: {
       query: {
          text: `SELECT * FROM "Student" WHERE rut = $1;`,
@@ -64,7 +56,6 @@ const queryConf = {
       },
       errMsg: `retrieving student`,
    },
-   // Edit config
    edit: {
       query: {
          text: `UPDATE "Student" SET nombre = $1, rut = $2, curso = $3, nivel = $4 WHERE rut = $2 RETURNING *;`,
@@ -83,26 +74,21 @@ const queryConf = {
 }
 
 module.exports = {
-   // Add student
    addStudent: async (values) => {
       queryConf.add.query.values = values
       await queryStructure(...Object.values(queryConf.add))
    },
-   // Get Students
    getStudents: async () => {
       await queryStructure(...Object.values(queryConf.get))
    },
-   // Get student by rut
    getStudentByRut: async (values) => {
       queryConf.getByRut.query.values = values
       await queryStructure(...Object.values(queryConf.getByRut))
    },
-   // Edit student
    editStudent: async (values) => {
       queryConf.edit.query.values = values
       await queryStructure(...Object.values(queryConf.edit))
    },
-   // Delete student
    deleteStudent: async (values) => {
       queryConf.delete.query.values = values
       await queryStructure(...Object.values(queryConf.delete))
